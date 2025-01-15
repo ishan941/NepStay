786ccfe086a9323fae005076c6d2e28f3c250b0b
Ishan Shrestha <iahan.esan18@gmail.com>
2025-01-15 19:40:49 +0545

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Categories />
         <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
           {" "}
-          <PropertyDetailPage />
+          <PropertyList />
         </div>
       </div>
     </main>
