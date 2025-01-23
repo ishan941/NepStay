68312568041443918ea1e129fad886bf6df8c179
Ishan Shrestha <iahan.esan18@gmail.com>
2025-01-23 10:52:18 +0545

    signup completed

---
+++ app/lib/actions.ts
@@ -0,0 +1,28 @@
+'use server'
+
+
+import { cookies } from "next/headers"
+
+export async function handleLogin(userId: string, accessToken: string, refreshToken: string){
+     (await cookies()).set('session_userid', userId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7, // One week
+        path: '/'
+    });
+     (await cookies()).set('session_access_token', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60, // 60 minitues
+        path: '/'
+    });
+     (await cookies()).set('session_userid', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7, // One week
+        path: '/'
+    });
+
+
+
+}
