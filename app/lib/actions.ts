805f439bfe5ff240d6a4a42d55b8248b01ef05a7
Ishan Shrestha <iahan.esan18@gmail.com>
2025-01-27 18:13:21 +0545

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,13 +16,25 @@ export async function handleLogin(userId: string, accessToken: string, refreshTo
         maxAge: 60 * 60, // 60 minitues
         path: '/'
     });
-     (await cookies()).set('session_userid', refreshToken, {
+     (await cookies()).set('session_refresh_token', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 7, // One week
         path: '/'
     });
-
-
-
 }
+
+
+export async function resetAuthCookies() {
+    (await cookies()).set('session_userid','');
+    (await cookies()).set('session_access_token','');
+    (await cookies()).set('session_refresh_token','');
+}
+
+//
+// Get data
+export async function getUserId() {
+    const userId = (await cookies()).get("session_userid")?.value;
+    return userId ? userId : null;
+  }
+  
