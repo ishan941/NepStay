d23eace61739df4904a0ccadd6abed6e0372c53f
Ishan Shrestha <iahan.esan18@gmail.com>
2025-02-01 17:53:51 +0545

    feat: Implemented Reservation

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,9 +1,11 @@
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+import { getUserId } from "@/app/lib/actions";
 import apiService from "@/app/services/apiService";
 import Image from "next/image";
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
+  const userId = await getUserId();
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       {" "}
@@ -41,7 +43,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
           <hr />
           <p className="mt-6 text-lg">{property.description}</p>
         </div>
-        <ReservationSidebar property={property} />
+        <ReservationSidebar userId={userId} property={property} />
       </div>
     </main>
   );
