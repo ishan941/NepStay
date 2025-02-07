7d3796392f77a3085ee23ff5f1d7b24d872ef9c6
Ishan Shrestha <iahan.esan18@gmail.com>
2025-02-07 21:49:14 +0545

    fix signup page

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import React from "react";
+import Modal from "./Modal";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useSignupModal from "@/app/hooks/useSignupModal";
-import Modal from "./Modal";
 import CustomButton from "../forms/CustomButton";
 import apiService from "@/app/services/apiService";
 import { handleLogin } from "@/app/lib/actions";
 
 const SignupModal = () => {
   //
-  // variables
+  // Variables
 
-  const signupModal = useSignupModal();
   const router = useRouter();
+  const signupModal = useSignupModal();
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
@@ -36,7 +36,7 @@ const SignupModal = () => {
     );
 
     if (response.access) {
-      handleLogin(response.user.pk, response.access, response.refresh); //properkey
+      handleLogin(response.user.pk, response.access, response.refresh);
 
       signupModal.close();
 
@@ -52,33 +52,35 @@ const SignupModal = () => {
 
   const content = (
     <>
-      <h2 className="mb-6 text-2xl">Welcome to NepStay, Please login</h2>
       <form action={submitSignup} className="space-y-4">
         <input
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Your email address"
+          placeholder="Your e-mail address"
           type="email"
-          className="w-full h-[54px] px-4 border rounded-xl"
+          className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
         />
+
         <input
           onChange={(e) => setPassword1(e.target.value)}
           placeholder="Your password"
           type="password"
-          className="w-full h-[54px] px-4 border rounded-xl"
+          className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
         />
+
         <input
           onChange={(e) => setPassword2(e.target.value)}
-          placeholder="Re-entered password"
+          placeholder="Repeat password"
           type="password"
-          className="w-full h-[54px] px-4 border rounded-xl"
+          className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
         />
-        {errors.map((errors, index) => {
+
+        {errors.map((error, index) => {
           return (
             <div
               key={`error_${index}`}
-              className="p-5 bg-airbnb text-white rounded-xl opacity-85"
+              className="p-5 bg-airbnb text-white rounded-xl opacity-80"
             >
-              {errors}
+              {error}
             </div>
           );
         })}
@@ -87,11 +89,12 @@ const SignupModal = () => {
       </form>
     </>
   );
+
   return (
     <Modal
       isOpen={signupModal.isOpen}
       close={signupModal.close}
-      lable="Signup"
+      lable="Sign up"
       content={content}
     />
   );
